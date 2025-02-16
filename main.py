7c9b5dd9768dbb89c113ccbfcea6f9a95927fc3c
Kiko Barr <34662575+kikobarr@users.noreply.github.com>
2025-02-15 20:10:43 -0800

    secret

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import os
 from dotenv import load_dotenv
+import openai
 from openai import OpenAI
 import time
 import logging
 import streamlit as st
 
 load_dotenv()
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 # Create an instance of the OpenAI API
 client = OpenAI()
 
